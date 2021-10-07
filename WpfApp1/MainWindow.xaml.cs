2548e0921a733e66f059df1749b22d4c61eae608
DESKTOP-ARONJ2L\Anton <max.verzilov32@gmail.com>
2021-10-07 15:43:23 +0500

    Глянуть и поменять дома

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,17 +12,47 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+  
     public partial class MainWindow : Window
     {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        public class Event
+        {
+            public int id { get; set; }
+            public int event_type_id { get; set; }
+            public int workshop_id { get; set; }
+            public int machine_id { get; set; }
+            public object time1 { get; set; }
+            public object time2 { get; set; }
+            public object data { get; set; }
+            public double @float { get; set; }
+            public object enum_value { get; set; }
+            public object uint32 { get; set; }
+        }
+
+        public class Payload
+        {
+            public List<Event> events { get; set; }
+            public string _rk { get; set; }
+        }
+
+        public class Root
+        {
+            public string command { get; set; }
+            public Payload payload { get; set; }
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            dynamic Event = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Event>>(resultString);
+            var result = new ConfigUrlsModel { ConfigUrls = configUrls };
         }
     }
 }
